af011b1b20ba8fd7378df1d8a65941659f5f1515
Georg Hagen <georg@hagen.at>
2014-06-07 23:30:48 +0200

    Option to disabel range checks

--- src/at/pcgamingfreaks/georgh/MarriageMaster/MarriageMaster.java
+++ src/at/pcgamingfreaks/georgh/MarriageMaster/MarriageMaster.java
@@ -157,7 +157,7 @@ public class MarriageMaster extends JavaPlugin
 		Location pl = player.getLocation();
 		Location opl = otherPlayer.getLocation();
 		
-		if(pl.getWorld().equals(opl.getWorld()) && pl.distance(opl) <= radius)
+		if(pl.getWorld().equals(opl.getWorld()) && (pl.distance(opl) <= radius || radius <= 0))
 		{
 			return true;
 		}
