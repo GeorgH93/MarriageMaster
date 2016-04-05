24ba4fa45831b0ec579fa13ae93b39f5b1edfd84
GeorgH93 <georg@hagen.at>
2016-04-05 14:30:13 +0200

    Add warning for old MinePacks versions

--- src/at/pcgamingfreaks/MarriageMaster/Bukkit/Minepacks/MinePacksIntegrationOld.java
+++ src/at/pcgamingfreaks/MarriageMaster/Bukkit/Minepacks/MinePacksIntegrationOld.java
@@ -1,23 +1,24 @@
 /*
- * Copyright (C) 2014-2015 GeorgH93
+ *   Copyright (C) 2015-2016 GeorgH93
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package at.pcgamingfreaks.MarriageMaster.Bukkit.Minepacks;
 
 import at.pcgamingfreaks.georgh.MinePacks.MinePacks;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
@@ -36,6 +37,7 @@ public class MinePacksIntegrationOld extends MinePacksIntegrationBase
 		if (mpProvider != null)
 		{
 			minepacks = mpProvider.getProvider();
+			System.out.println("You are using an outdated version of MinePacks! Please update it!!!");
 		}
 	}
 
