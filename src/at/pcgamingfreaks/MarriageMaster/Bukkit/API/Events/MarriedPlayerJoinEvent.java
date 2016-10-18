505eb659f80e37ee956308a05aa03101ca3ed362
GeorgH93 <georg@hagen.at>
2016-10-19 00:05:44 +0200

    Add new events

---
+++ src/at/pcgamingfreaks/MarriageMaster/Bukkit/API/Events/MarriedPlayerJoinEvent.java
@@ -0,0 +1,59 @@
+/*
+ *   Copyright (C) 2016 GeorgH93
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.pcgamingfreaks.MarriageMaster.Bukkit.API.Events;
+
+import at.pcgamingfreaks.MarriageMaster.Bukkit.API.MarriagePlayer;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Event is fired when a married player joins the server.
+ */
+@SuppressWarnings("unused")
+public class MarriedPlayerJoinEvent extends Event
+{
+	private MarriagePlayer player;
+
+	public MarriedPlayerJoinEvent(MarriagePlayer player)
+	{
+		this.player = player;
+	}
+
+	/**
+	 * @return The married player that has joined the server
+	 */
+	public MarriagePlayer getPlayer()
+	{
+		return this.player;
+	}
+
+	// Bukkit handler stuff
+	private static final HandlerList handlers = new HandlerList();
+
+	@Override
+	public HandlerList getHandlers()
+	{
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList()
+	{
+		return handlers;
+	}
+}
