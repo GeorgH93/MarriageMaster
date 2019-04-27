2fff191377c48cdb97ad89c8695ec3f45dbc9f2c
GeorgH93 <git@georgh93.eu>
2019-04-28 00:28:56 +0200

    Remove support for DeluxeChat placeholder api DeluxeChat no longer has it's own placeholder api so this is no longer needed

--- src/at/pcgamingfreaks/MarriageMaster/Bukkit/Placeholder/PlaceholderManager.java
+++ src/at/pcgamingfreaks/MarriageMaster/Bukkit/Placeholder/PlaceholderManager.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2016 GeorgH93
+ *   Copyright (C) 2019 GeorgH93
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -19,7 +19,6 @@ package at.pcgamingfreaks.MarriageMaster.Bukkit.Placeholder;
 
 import at.pcgamingfreaks.MarriageMaster.Bukkit.MarriageMaster;
 import at.pcgamingfreaks.MarriageMaster.Bukkit.Placeholder.Hooks.ClipsPlaceholderHook;
-import at.pcgamingfreaks.MarriageMaster.Bukkit.Placeholder.Hooks.DeluxeChatPlaceholderHook;
 import at.pcgamingfreaks.MarriageMaster.Bukkit.Placeholder.Hooks.MVdWPlaceholderReplacer;
 import at.pcgamingfreaks.MarriageMaster.Bukkit.Placeholder.Hooks.PlaceholderAPIHook;
 import at.pcgamingfreaks.MarriageMaster.Bukkit.Placeholder.Replacer.*;
@@ -44,12 +43,6 @@ public class PlaceholderManager
 		this.plugin = plugin;
 		if(!(isPluginEnabled("DeluxeChat") || isPluginEnabled("MVdWPlaceholderAPI") || isPluginEnabled("PlaceholderAPI"))) return; // No supported placeholder API installed
 		generatePlaceholdersMap();
-		//region DeluxeChat
-		if(isPluginEnabled("DeluxeChat"))
-		{
-			hooks.add(new DeluxeChatPlaceholderHook(plugin, this));
-		}
-		//endregion
 		//region MVdW Placeholders
 		if(isPluginEnabled("MVdWPlaceholderAPI"))
 		{
