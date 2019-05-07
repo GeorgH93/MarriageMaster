a3ada4bf51c2d5a8f3cf0a34c2be03acfa1445b8
GeorgH93 <git@georgh93.eu>
2019-05-07 18:52:23 +0200

    Add interface for faster inRangeSquared test

--- src/at/pcgamingfreaks/MarriageMaster/Bukkit/API/Marriage.java
+++ src/at/pcgamingfreaks/MarriageMaster/Bukkit/API/Marriage.java
@@ -37,4 +37,13 @@ public interface Marriage extends at.pcgamingfreaks.MarriageMaster.API.Marriage<
 	 * @return True if they are within the given distance. False if not or one is offline or both of them are offline.
 	 */
 	boolean inRange(double maxDistance);
+
+	/**
+	 * Checks if both players are within a given distance.
+	 *
+	 * @param maxDistanceSquared The maximum squared distance between both players.
+	 *                           -1 no range limit and no world limit, 0 no range limit but in the same world, greater than 0 range limit in meters/blocks.
+	 * @return True if they are within the given distance. False if not or one is offline or both of them are offline.
+	 */
+	boolean inRangeSquared(double maxDistanceSquared);
 }
