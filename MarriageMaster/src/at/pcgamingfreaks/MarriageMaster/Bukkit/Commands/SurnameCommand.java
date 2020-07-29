b71648b831ad24b45e4e56a950159cea7bc629be
GeorgH93 <git@georgh93.eu>
2020-07-29 17:27:29 +0200

    Fix error when using the surname command without any arguments

--- MarriageMaster/src/at/pcgamingfreaks/MarriageMaster/Bukkit/Commands/SurnameCommand.java
+++ MarriageMaster/src/at/pcgamingfreaks/MarriageMaster/Bukkit/Commands/SurnameCommand.java
@@ -56,6 +56,11 @@ public class SurnameCommand extends MarryCommand
 	@Override
 	public void execute(@NotNull CommandSender sender, @NotNull String mainCommandAlias, @NotNull String alias, @NotNull String[] args)
 	{
+		if(args.length == 0)
+		{
+			showHelp(sender, mainCommandAlias);
+			return;
+		}
 		final String newSurname = args[args.length - 1];
 		MarriagePlayer player = (sender instanceof Player) ? getMarriagePlugin().getPlayerData((Player) sender) : null;
 		if(!(sender instanceof Player) || player.isPriest() || getMarriagePlugin().isSelfMarriageAllowed() && sender.hasPermission(Permissions.SELF_MARRY) && player.isMarried())
