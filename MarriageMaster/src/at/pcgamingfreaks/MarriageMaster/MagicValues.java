70402abf8c61037f5e4bdd032614ccf8a51bbf91
GeorgH93 <git@georgh93.eu>
2021-05-16 17:21:56 +0200

    Update PCGF PluginLib

--- MarriageMaster/src/at/pcgamingfreaks/MarriageMaster/MagicValues.java
+++ MarriageMaster/src/at/pcgamingfreaks/MarriageMaster/MagicValues.java
@@ -24,5 +24,5 @@ public class MagicValues
 	public static final int LANG_VERSION = 105, LANG_PRE_V2_VERSIONS = 90;
 	public static final int CONFIG_VERSION = 103, BUNGEE_CONFIG_VERSION = 102, CONFIG_PRE_V2_VERSIONS = 90;
 	public static final String SYMBOL_HEART = "\u2764", SYMBOL_SMALL_HEART = "\u2665", HEART_AND_RESET = SYMBOL_HEART + MessageColor.WHITE, RED_HEART = MessageColor.RED + HEART_AND_RESET;
-	public static final String MIN_PCGF_PLUGIN_LIB_VERSION = "1.0.31-SNAPSHOT";
+	public static final String MIN_PCGF_PLUGIN_LIB_VERSION = "1.0.32-SNAPSHOT";
 }
